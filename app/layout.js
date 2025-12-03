22873c8ad45cd53402d86a3e6b35838b40c9d9a1
Jeffy2600II <nontakorn2600@gmail.com>
2025-12-03 11:13:12 +0700

    update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,32 @@
-export const metadata = {
-  title: 'RoutineOS',
-  description: 'กิจวัตรประจำวันส่วนตัว',
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="th">
       <head>
         <link rel="manifest" href="/manifest.json" />
+        {/* เพิ่ม script ตรวจ version */}
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            (function() {
+              var LOCAL_VERSION = window.localStorage.getItem("ROUTINEOS_VERSION");
+              function checkManifestVersion() {
+                fetch("/manifest.json")
+                  .then(res => res.json())
+                  .then(data => {
+                    if (data.version) {
+                      if (!LOCAL_VERSION) {
+                        window.localStorage.setItem("ROUTINEOS_VERSION", data.version);
+                      } else if (LOCAL_VERSION !== data.version) {
+                        window.localStorage.setItem("ROUTINEOS_VERSION", data.version);
+                        location.reload(true);
+                      }
+                    }
+                  });
+              }
+              setInterval(checkManifestVersion, 30000); // เช็คทุก 30 วินาที
+              checkManifestVersion(); // เช็คทันที
+            })();
+          `
+        }} />
       </head>
       <body style={{ fontFamily: "sans-serif", padding: "20px" }}>
         {children}
